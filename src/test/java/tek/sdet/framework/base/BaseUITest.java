89f6b1e2e72ddacd467be9351a8243ab659730db
NaveedSar <Naveed.s100@gmail.com>
2023-10-27 08:47:55 -0400

    fixing the issue

--- src/test/java/tek/sdet/framework/base/BaseUITest.java
+++ src/test/java/tek/sdet/framework/base/BaseUITest.java
@@ -5,19 +5,18 @@ import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
 import tek.sdet.framework.utilities.CommonUtility;
 
-public class BaseUITest extends  CommonUtility{
-	
-	
+public class BaseUITest extends CommonUtility {
+
 	@Before
 	public void setupTest() {
 		super.setupBrowser();
 	}
-	
+
 	@After
 	public void closeTests(Scenario scenario) {
 		if (scenario.isFailed()) {
 			byte[] screenShot = takeScreenShotAsBytes();
-			scenario.attach(screenShot, "image/png" , scenario.getName() +  " Scenario Failed");
+			scenario.attach(screenShot, "image/png", scenario.getName() + " Scenario Failed");
 		}
 		super.quitBrowser();
 	}
